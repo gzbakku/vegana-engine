ae6291f303a7947bd6c75213d103bd22d05b6a4f
gzbakku <gzbakku@gmail.com>
2020-04-20 20:56:01 +0530

    data api configured to preserve data types of the data saved

--- data.js
+++ data.js
@@ -1,8 +1,18 @@
 var db = {};
+let type_collection;
 
 module.exports = {
 
   get : function(tag,where){
+
+    if(!type_collection){
+      process_type_collection();
+    }
+
+    let data_type;
+    if(where === "local" || where === "session"){
+      data_type = type_collection[where][tag];
+    }
 
     if(typeof(tag) !== 'string'){
       return engine.common.error('invalid_tag');
@@ -22,14 +32,14 @@ module.exports = {
       if(sessionStorage.hasOwnProperty(tag) == false){
         return null;
       } else {
-        return sessionStorage[tag];
+        return convert(data_type,sessionStorage[tag]);
       }
     }
     if(where == 'local'){
       if(localStorage.hasOwnProperty(tag) == false){
         return null;
       } else {
-        return localStorage[tag];
+        return convert(data_type,localStorage[tag]);
       }
     }
 
@@ -43,37 +53,38 @@ module.exports = {
     if(!where){
       where = 'mem';
     }
+    let parsed;
     if(typeof(value) == 'object'){
-      value = JSON.stringify(value);
-    }
-    if(typeof(value) !== 'string'){
-      value = value.toString();
+      parsed = JSON.stringify(value);
+    } else if(typeof(value) !== 'string') {
+      parsed = value.toString();
+    } else {
+      parsed = value;
     }
 
     if(where == 'mem'){
       if(!db[tag]){
-        db[tag] = value;
-        return true;
+        db[tag] = parsed;
       } else {
         return false;
       }
     }
     if(where == 'session'){
       if(!sessionStorage[tag]){
-        sessionStorage.setItem(tag,value);
-        return true;
+        sessionStorage.setItem(tag,parsed);
       } else {
         return false;
       }
     }
     if(where == 'local'){
       if(!localStorage[tag]){
-        localStorage.setItem(tag,value);
-        return true;
+        localStorage.setItem(tag,parsed);
       } else {
         return false;
       }
     }
+
+    save_data_type(tag,where,typeof(value));
 
   },
 
@@ -85,26 +96,90 @@ module.exports = {
     if(!where){
       where = 'mem';
     }
+    let parsed;
     if(typeof(value) == 'object'){
-      value = JSON.stringify(value);
-    }
-    if(typeof(value) !== 'string'){
-      value = value.toString();
+      parsed = JSON.stringify(value);
+    } else if(typeof(value) !== 'string') {
+      parsed = value.toString();
+    } else {
+      parsed = value;
     }
 
     if(where == 'mem'){
       db[tag] = value;
-      return true;
     }
     if(where == 'session'){
-      sessionStorage.setItem(tag,value);
-      return true;
+      sessionStorage.setItem(tag,parsed);
     }
     if(where == 'local'){
-      localStorage.setItem(tag,value);
-      return true;
+      localStorage.setItem(tag,parsed);
     }
+
+    save_data_type(tag,where,typeof(value));
+
+    return true;
 
   }
 
+}
+
+function convert(type,data){
+  if(type == "number"){
+    return Number(data);
+  }
+  if(type == "object"){
+    return JSON.parse(data);
+  }
+  if(type == "boolean"){
+    return JSON.parse(data);
+  }
+  return data;
+}
+
+function save_data_type(tag,where,type){
+
+  if(!type_collection){
+    process_type_collection();
+  }
+
+  if(where === "local"){
+    type_collection.local[tag] = type;
+    localStorage.setItem("type_collection_local",JSON.stringify(type_collection.local));
+  }
+  if(where === "session"){
+    type_collection.session[tag] = type;
+    localStorage.setItem("type_collection_session",JSON.stringify(type_collection.session));
+  }
+
+  return true;
+
+}
+
+function process_type_collection(){
+  let get_local,get_session;
+  try{
+    const fetch = localStorage.getItem("type_collection_session");
+    if(fetch){
+      get_session = JSON.parse(fetch)
+    } else {
+      get_session = {};
+    }
+  } catch {
+    get_session = {}
+  }
+  try{
+    const fetch = localStorage.getItem("type_collection_local");
+    if(fetch){
+      get_local = JSON.parse(fetch)
+    } else {
+      get_local = {};
+    }
+  } catch {
+    get_local = {}
+  }
+  type_collection = {
+    session:get_session,
+    local:get_local
+  };
+  return true;
 }
