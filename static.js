92bf1ea27b3f1c6e6a61666e590e40a662318ba9
gzbakku <>
2021-02-27 04:16:22 +0530

    static add apis added sorry guys

--- static.js
+++ static.js
@@ -2,5 +2,11 @@
 
 module.exports = {
   onWeb:()=>{return true;},
-  publish:()=>{return true;}
+  publish:()=>{return true;},
+  add:{
+    globalComp:()=>{return true;},
+    page:()=>{return true;},
+    cont:()=>{return true;},
+    panel:()=>{return true;},
+  },
 };
