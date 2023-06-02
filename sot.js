27110dc159d4684465f581be6a7b8154a2c8ea6c
gzbakku <gzbakku@gmail.com>
2023-06-02 22:03:59 +0530

    sot is updated

--- sot.js
+++ sot.js
@@ -1,23 +1,51 @@
 
 
 module.exports = {
-    new:new_sot
+    new:new_sot,
+    id:()=>{
+        let id = engine.uniqid();
+        if(!engine.sote_events){
+            engine.sote_events = {};
+        }
+        engine.sote_events[id] = build();
+        return id;
+    },
+    get:(id)=>{
+        return engine.sote_events[id];
+    }
 };
 
 function new_sot(){
+    let id = engine.uniqid();
+    if(!engine.sote_events){
+        engine.sote_events = {};
+    }
+    engine.sote_events[id] = build();
+    return engine.sote_events[id];
+}
+
+function build(){
+
+    if(!engine.sote_events){
+        engine.sote_events = {};
+        // engine.sote_events_data = {};
+    }
 
     let object = {};
     let functions = [];
-
+    
     function update(){
         for(let func of functions){
             if(typeof(func) === "function"){func();}
         }
     }
 
-    return {
+    let builder = {
         listen:(func)=>{
-            if(typeof(func) === "function"){functions.push(func);return true;} 
+            if(typeof(func) === "function"){
+                functions.push(func);
+                return true;
+            } 
             return false;
         },
         get:(key)=>{
@@ -32,8 +60,13 @@ function new_sot(){
             object = value;
             update();
             return true;
+        },
+        delete:()=>{
+            delete engine.sote_events[id];
         }
     };
+
+    return builder;
 
 }
 
