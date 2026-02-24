cbe01eddcbf12556575bd0e6ad917e1ae2057e71
akku <gzbakku@gmail.com>
2026-02-24 15:44:58 +0530

    view can now use custom display values for hide and show actions

--- view.js
+++ view.js
@@ -6,9 +6,19 @@ function doo(id,what){
   if(get == null){
     return false;
   }
+  let og = get.style.display;
   if(what == 'show'){
-    get.style.display = 'block';
+    let og_view = get.ogDisplayValue;
+    if(typeof(og_view) === "string" && og_view.length > 0){
+      get.style.display = og_view;
+    } 
+    else {
+      get.style.display = 'block';
+    }
   } else if (what == 'hide'){
+    if(typeof(og) === "string" && og.length > 0){
+      get.ogDisplayValue = og;
+    }
     get.style.display = 'none';
   } else if (what == 'remove'){
     get.remove();
